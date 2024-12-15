284d59078ac4697c68db709c47d8e36be4ee987a
Shiphan <140245703+Shiphan@users.noreply.github.com>
2024-12-16 01:39:28 +0800

    remove testing code

--- src/App.java
+++ src/App.java
@@ -1,9 +1,7 @@
-import java.util.Arrays;
 import java.util.List;
 
 import example.Example;
 import markdownDocument.MarkdownDocument;
-import markdownDocument.Util;
 import markdownDocument.block.Heading.Level;
 import markdownDocument.block.Heading;
 import markdownDocument.block.HorizontalRule;
@@ -11,12 +9,8 @@ import markdownDocument.block.Quote;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSeparator;
-import javax.swing.JTextPane;
-import javax.swing.text.BadLocationException;
 
 public class App {
 	public static void main(String[] args) {
@@ -28,26 +22,22 @@ public class App {
 
 		var markdownDocument = new MarkdownDocument(
 			List.of(
-				/*
 				new Heading("test here", Level.h1),
 				new Heading("test here", Level.h2),
 				new Heading("test here", Level.h3),
 				new Heading("test here", Level.h4),
 				new Heading("test here", Level.h5),
-				new Heading("test here", Level.h6)
-				*/
-				new HorizontalRule()
-				/*
+				new Heading("test here", Level.h6),
+				new HorizontalRule(),
 				new Quote(List.of(
 					new Heading("test", Level.h3),
 					new Quote(List.of(
-						new Heading("test2", Level.h5)
-						//new HorizontalRule()
-					))
-					//new HorizontalRule(),
-					//new HorizontalRule()
+						new Heading("test2", Level.h5),
+						new HorizontalRule()
+					)),
+					new HorizontalRule(),
+					new HorizontalRule()
 				))
-				*/
 			)
 		);
 
@@ -55,34 +45,6 @@ public class App {
 		// panel.add(markdownDocument.render());
 		// final var renderResult = markdownDocument.render();
 		panel.add(new JScrollPane(markdownDocument.render()));
-		//panel.add(markdownDocument.render());
-
-		//panel.add(new JScrollPane(new MarkdownDocument(List.of(
-		//	new Heading("Test heading", Level.h2),
-		//	new Heading("Test heading", Level.h2)
-		//)).render()));
-
-		final var testPane = new JTextPane();
-		final var doc = testPane.getDocument();
-		try {
-			doc.insertString(doc.getLength(), "LKSJDFLKAJLSKDJALKJDLWKAJLK\n", null);
-
-			Util.setSelectionToEnd(testPane);
-			testPane.insertComponent(new JLabel("abc"));
-			doc.insertString(doc.getLength(), "\n", null);
-
-			doc.insertString(doc.getLength(), "LKSJDFLKAJLSKDJALKJDLWKAJLK\n", null);
-
-			Util.setSelectionToEnd(testPane);
-			testPane.insertComponent(new JSeparator());
-			doc.insertString(doc.getLength(), "\n", null);
-
-			Util.setSelectionToEnd(testPane);
-			testPane.insertComponent(new JLabel("abc"));
-		} catch (BadLocationException e) {
-			throw new RuntimeException(e);
-		}
-		//panel.add(new JScrollPane(testPane));
 
 		frame.add(panel);
 
